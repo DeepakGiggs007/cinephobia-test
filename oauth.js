b68f4795d9353c04fa4fbc370749ae7c71ab2109
DeepakGiggs <deepakthegiggs@outlook.com>
2015-10-03 22:07:04 +0530

    1st commit

---
+++ oauth.js
@@ -0,0 +1,19 @@
+// You need to add your Facebook and Twitter keys here
+// https://dev.twitter.com/
+// https://developers.facebook.com/
+var ids = {
+    facebook: {
+        clientID: "916451825115127",
+        clientSecret: "1f359eb4f20af0e129b57e06b2bdddb2",
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+        consumerKey: "MHmt83oTXKQHOsySs7rUP5CPl",
+        consumerSecret: "epGarXSQbhRgUromQuUAydoCspCxPxnCPhbsRWPtuGNRseMq7x",
+        callbackURL: "http://localhost:3000/auth/twitter/callback",
+        accessToken: "3569919499-Gk3BUPBQ5UqfzMk22dCRu6cEjtDaLBNCqbF23sS",
+        accessTokenSecret: "klNsTC2lXUp4wryXTulZf4EpWgXu8T4jYI6ND43NMzS6B"
+    }
+}
+
+module.exports = ids
